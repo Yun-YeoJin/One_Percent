1d0362d57c66c6fb0e80bcbba3af4b23602051b2
YunYeoJin <duwls0349@naver.com>
2022-09-10 20:09:12 +0900

    Feat: Constant & Transition+Extension 생성

---
+++ One_Percent/Extension/Transition+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  Transition+Extension.swift
+//  One_Percent
+//
+//  Created by 윤여진 on 2022/09/10.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    enum TransitionStyle {
+        case present //네비게이션 없이 present
+        case presentNavigation //네비게이션 임베드 present
+        case presentFullNavigation //네비게이션 풀스크린
+        case push
+    }
+    
+    func transition<T: UIViewController>(_ viewController: T, transitionStyle: TransitionStyle = .present) {
+        
+        switch transitionStyle {
+        case .present:
+            self.present(viewController, animated: true) //self.present(T(), animated: true)와 차이.
+        case .presentNavigation:
+            let navi = UINavigationController(rootViewController: viewController)
+            self.present(navi, animated: true)
+        case .presentFullNavigation:
+            let navi = UINavigationController(rootViewController: viewController)
+            navi.modalPresentationStyle = .fullScreen
+            self.present(navi, animated: true)
+        case .push:
+            self.navigationController?.pushViewController(viewController, animated: true)
+        
+        }
+    }
+}
---
+++ One_Percent/Utility/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  One_Percent
+//
+//  Created by 윤여진 on 2022/09/10.
+//
+
+import UIKit
+
+enum Constants {
+    
+    enum Design {
+        static let cornerRadius: CGFloat = 10
+        static let borderWidth: CGFloat = 1
+    }
+}
+
+extension Constants {
+    
+    enum BaseColor {
+        static let background = UIColor.systemBackground
+        static let border = UIColor.label.cgColor
+        static let text = UIColor.label
+        static let placeholder = UIColor.systemGray
+        static let point = UIColor.systemOrange
+    }
+    
+}
